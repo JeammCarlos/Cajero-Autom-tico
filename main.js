5b2c20691d8a332c8436e93bc10aa3e97f10b23e
JeammCarlos <jeamcr.murillo@hotmail.com>
2023-12-18 13:44:01 -0600

    Cajero Automático

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function validateUser() {
 }
 
 function checkBalance() {
-    document.getElementById("output").innerHTML = `Saldo actual actual: $${selectedAccount.saldo}`;
+    document.getElementById("output").innerHTML = `<h3>Saldo actual:</h3><p class="text-info">$${selectedAccount.saldo}</p>`;
 }
 
 function deposit() {
@@ -37,10 +37,10 @@ function deposit() {
     selectedAccount.saldo += amount;
 
     if (selectedAccount.saldo > 990) {
-        alert(`No puedes tener más de $990. El monto máximo permitido es $${990 - amount}.`);
+        alert(`No puedes tener más de $990. El monto máximo que podrías ingresar es $${990 - (selectedAccount.saldo - amount)}.`);
         selectedAccount.saldo -= amount;
     } else {
-        document.getElementById("output").innerHTML = `Monto ingresado: $${amount}<br>Saldo total actual: $${selectedAccount.saldo}`;
+        document.getElementById("output").innerHTML = `<h3>Monto ingresado:</h3><p class="success">+ $${amount}</p><h3>Saldo total actual:</h3><p class="text-info">$${selectedAccount.saldo}</p>`;
     }
 }
 
@@ -53,9 +53,18 @@ function withdraw() {
     }
 
     if (selectedAccount.saldo - amount < 10) {
-        alert("No puedes dejar menos de $10 en la cuenta.");
+        alert(`No puedes dejar menos de $10 en la cuenta. El monto máximo que podrías retirar es $${selectedAccount.saldo-10}.`);
     } else {
         selectedAccount.saldo -= amount;
-        document.getElementById("output").innerHTML = `Monto retirado: $${amount}<br>Saldo total actual: $${selectedAccount.saldo}`;
+        document.getElementById("output").innerHTML = `<h3>Monto retirado:</h3><p class="error">- $${amount}</p><h3>Saldo total actual:</h3><p class="text-info">$${selectedAccount.saldo}</p>`;
     }
+}
+
+function logout() {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("options").style.display = "none";
+    selectedAccount = null;
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    alert("Gracias por confiar en nosotros. ¡Te esperamos pronto!");
 }
